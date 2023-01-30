fb077e8cc824b579582c9f239d2560a400c071e7
Taylor Smith <smith.taylord@gmail.com>
2023-01-30 15:21:03 -0700

    Created Players MVCSS

---
+++ app/App.js
@@ -0,0 +1,9 @@
+import { ValuesController } from "./Controllers/ValuesController.js";
+
+class App {
+  // valuesController = new ValuesController(); not needed for this project
+
+
+}
+
+window["app"] = new App();
---
+++ app/Models/Player.js
@@ -0,0 +1,7 @@
+export class Player {
+    constructor(name) {
+        this.name = name;
+        this.score = 0
+    }
+
+}
